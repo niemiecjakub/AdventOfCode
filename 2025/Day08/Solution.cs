0bf4ecf5c2dc8571d53e46d0e1dc77d776266294
niemiecjakub <kniemiec3@gmail.com>
2025-12-29 22:08:22 +0100

    Day 9 part two

--- 2025/Day08/Solution.cs
+++ 2025/Day08/Solution.cs
@@ -19,8 +19,7 @@ public class Solution
       {
         var p1 = positions[i];
         var p2 = positions[j];
-        var dist = Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2) + Math.Pow(p1.Z - p2.Z, 2));
-        edges.Add(new Edge(p1, p2, dist));
+        edges.Add(new Edge(p1, p2));
       }
     }
 
@@ -30,11 +29,8 @@ public class Solution
     for (int i = 0; i < 1000; i++)
     {
       var edge = edgesSorted[i];
-      unionFind.Union(edge.P1, edge.P2);
+      unionFind.Union(edge);
     }
-
-    var circuits2 = positions
-     .GroupBy(unionFind.Find);
 
     var circuits = positions
         .GroupBy(unionFind.Find)
@@ -52,12 +48,63 @@ public class Solution
     Console.WriteLine($"Result: {result}");
   }
 
-  private record Edge(Position P1, Position P2, double Distance);
-  private record Position(int X, int Y, int Z);
+  public static void PartTwo()
+  {
+    var positions = File.ReadAllLines("2025/Day08/input.txt")
+      .Select(l =>
+      {
+        var coords = l.Split(',');
+        return new Position(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
+      })
+      .ToList();
+
+    var edges = new List<Edge>();
+    for (int i = 0; i < positions.Count; i++)
+    {
+      for (int j = i + 1; j < positions.Count; j++)
+      {
+        var p1 = positions[i];
+        var p2 = positions[j];
+        edges.Add(new Edge(p1, p2));
+      }
+    }
+
+    var edgesSorted = edges.OrderBy(e => e.Distance).ToList();
+    var unionFind = new UnionFind(positions);
+
+    for (int i = 0; i < edgesSorted.Count; i++)
+    {
+      var edge = edgesSorted[i];
+      unionFind.Union(edge);
+
+      if (unionFind.UnionCount == positions.Count - 1)
+      {
+        long result = edge.P1.X * edge.P2.X;
+        Console.WriteLine($"Result: {result}");
+        break;
+      }
+    }
+  }
+
+  private record Edge
+  {
+    public Position P1;
+    public Position P2;
+    public double Distance;
+    public Edge(Position p1, Position p2)
+    {
+      P1 = p1;
+      P2 = p2;
+      Distance = Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2) + Math.Pow(p1.Z - p2.Z, 2));
+    }
+  }
+
+  private record Position(long X, long Y, long Z);
+
   private class UnionFind
   {
     private Dictionary<Position, Position> _parent = new();
-
+    public int UnionCount { get; private set; } = 0;
     public UnionFind(List<Position> positions)
     {
       foreach (var p in positions)
@@ -75,17 +122,19 @@ public class Solution
       return _parent[p];
     }
 
-    public void Union(Position p1, Position p2)
+    public bool Union(Edge edge)
     {
-      var root1 = Find(p1);
-      var root2 = Find(p2);
+      var root1 = Find(edge.P1);
+      var root2 = Find(edge.P2);
 
       if (root1 == root2)
       {
-        return;
+        return false;
       }
 
+      UnionCount++;
       _parent[root1] = root2;
+      return true;
     }
   }
 }
--- 2025/Day09/Solution.cs
+++ 2025/Day09/Solution.cs
@@ -14,7 +14,6 @@ public class Solution
     long area = long.MinValue;
     for (int i = 0; i < coordinates.Count; i++)
     {
-
       for (int j = i + 1; j < coordinates.Count; j++)
       {
         var rectangle = new Rectangle(coordinates[i], coordinates[j]);
@@ -30,19 +29,107 @@ public class Solution
 
   public static void PartTwo()
   {
+    var redTiles = File.ReadAllLines("2025/Day09/input.txt")
+      .Select(l =>
+      {
+        var p = l.Split(',');
+        return new Point(int.Parse(p[0]), int.Parse(p[1]));
+      })
+      .ToList();
 
+    var greenTiles = new HashSet<Point>();
+    for (int i = 0; i < redTiles.Count; i++)
+    {
+      var p1 = redTiles[i];
+      var p2 = redTiles[(i + 1) % redTiles.Count];
+
+      if (p1.X == p2.X)
+      {
+        int minY = Math.Min(p1.Y, p2.Y);
+        int maxY = Math.Max(p1.Y, p2.Y);
+        for (int y = minY + 1; y < maxY; y++)
+          greenTiles.Add(new Point(p1.X, y));
+      }
+      else if (p1.Y == p2.Y)
+      {
+        int minX = Math.Min(p1.X, p2.X);
+        int maxX = Math.Max(p1.X, p2.X);
+        for (int x = minX + 1; x < maxX; x++)
+          greenTiles.Add(new Point(x, p1.Y));
+      }
+    }
+
+    var boundaryTiles = redTiles.Concat(greenTiles).ToList();
+    var rowRanges = boundaryTiles
+      .GroupBy(p => p.Y)
+      .ToDictionary(
+        g => g.Key,
+        g => (Min: g.Min(p => p.X), Max: g.Max(p => p.X))
+      );
+
+    long area = 0;
+
+    for (int i = 0; i < redTiles.Count; i++)
+    {
+      for (int j = i + 1; j < redTiles.Count; j++)
+      {
+        var r = new Rectangle(redTiles[i], redTiles[j]);
+
+        if (r.Bounds.MinX == r.Bounds.MaxX || r.Bounds.MinY == r.Bounds.MaxY)
+        {
+          continue;
+        }
+
+        if (r.Area > area && IsRectangleInsidePolygon(r, rowRanges))
+        {
+          area = r.Area;
+        }
+      }
+    }
+
+    Console.WriteLine($"Max area is: {area}");
+  }
+
+
+  private static bool IsRectangleInsidePolygon(Rectangle r, Dictionary<int, (int Min, int Max)> rowRanges)
+  {
+    for (int y = r.Bounds.MinY; y <= r.Bounds.MaxY; y++)
+    {
+      if (!rowRanges.TryGetValue(y, out var span))
+      {
+        return false;
+      }
+
+      if (span.Min > r.Bounds.MinX)
+      {
+        return false;
+      }
+
+      if (span.Max < r.Bounds.MaxX)
+        return false;
+    }
+
+    return true;
   }
 
   private readonly record struct Rectangle
   {
     public long Area { get; }
+    public (int MinX, int MaxX, int MinY, int MaxY) Bounds { get; }
     public Rectangle(Point p1, Point p2)
     {
-      long width = Math.Abs(p1.X - p2.X) + 1;
-      long height = Math.Abs(p1.Y - p2.Y) + 1;
+      int minX = Math.Min(p1.X, p2.X);
+      int maxX = Math.Max(p1.X, p2.X);
+      int minY = Math.Min(p1.Y, p2.Y);
+      int maxY = Math.Max(p1.Y, p2.Y);
+
+      long width = maxX - minX + 1;
+      long height = maxY - minY + 1;
       Area = width * height;
+      Bounds = (minX, maxX, minY, maxY);
     }
   }
 
   private record struct Point(int X, int Y);
+
 }
